e347959e7b056f9f977595a9dd133234edf591d1
Ionitron <hi@ionicframework.com>
2023-01-12 09:39:47 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '__NAME__',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
